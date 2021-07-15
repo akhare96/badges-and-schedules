b8bdf1ad554ef55f66c08b8573ee64c95549cf19
Ambar Khare <ambar.khare24@gmail.com>
2021-07-15 10:53:55 -0700

    commit tree 14e8c6722b4a8613fc1b7f95f9f4a9ffe1c775e1

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(guests)
+    guests.collect.each {
+        |guest| "Hello, my name is #{guest}."
+    }
+end
+
+def assign_rooms(guests)
+    guests.collect.with_index(1) {
+        |guest, index| "Hello, #{guest}! You'll be assigned to room #{index}!"
+    }
+end
+
+def printer(guests)
+    batch_badge_creator(guests).each {
+        |tag| puts tag
+    }
+    assign_rooms(guests).each {
+        |room| puts room
+    }
+end
